ae1ddc369cf0d81cbba9129fa5cf53e13f17a2e7
James Z. Howes <zhowes14@icloud.com>
2020-12-06 23:57:20 -0500

    created function to find if the password is valid

--- adventOfCodeDay2/main.cpp
+++ adventOfCodeDay2/main.cpp
@@ -7,8 +7,25 @@
 
 #include <iostream>
 
+bool validPassword(int low, int high, std::string password, char requiredChar);
+
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	
 	return 0;
+}
+
+
+bool validPassword(int low, int high, std::string password, char requiredChar) {
+	int counter = 0;
+	for (int i = 0; i < password.size(); ++i) {
+		if (password[i] == requiredChar) {
+			++counter;
+		}
+	}
+	
+	if (low >= counter && counter <= high) {
+		return true;
+	}
+	
+	return false;
 }
